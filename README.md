9eb6ff662de07d648917828cf83fa51d5cd90f93
Christopher Chang <chiggiechang@gmail.com>
2020-01-29 11:41:02 +0000

    Change output formatting

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Christopher Chang
 
 I looked at the class_demo.h and class_demo.cpp examples from class as a
 reference.
+
+I looked at textbook section 9.2 to format output with <iomanip>.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "pet.h"
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,15 +9,17 @@ int main(int argc, char **argv)
   Pet *p1 = new Pet("Ulysses", 4, "dog", 63.7);
   Pet *p2 = new Pet("Pearl", 7, "cat", 9.4);
 
-  cout << p1->getName() << ':' << endl;
-  cout << "\tage: " << p1->getAge() << endl;
-  cout << "\ttype: " << p1->getType() << endl;
-  cout << "\tweight: " << p1->getWeight() << endl;
+  cout << p1->getName() << endl;
+  cout << setw(10) << "age: " << p1->getAge() << endl;
+  cout << setw(10) << "type: " << p1->getType() << endl;
+  cout << setw(10) << "weight: " << p1->getWeight() << endl;
 
-  cout << p2->getName() << ':' << endl;
-  cout << "\tage: " << p2->getAge() << endl;
-  cout << "\ttype: " << p2->getType() << endl;
-  cout << "\tweight: " << p2->getWeight() << endl;
+  cout << endl;
+
+  cout << p2->getName() << endl;
+  cout << setw(10) << "age: " << p2->getAge() << endl;
+  cout << setw(10) << "type: " << p2->getType() << endl;
+  cout << setw(10) << "weight: " << p2->getWeight() << endl;
 
   delete p1;
   delete p2;
